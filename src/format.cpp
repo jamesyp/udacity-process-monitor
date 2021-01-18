319e9cc1653d6cd990bfe8ed26aeea0d2e957137
James Patton <jamesyp@gmail.com>
2021-01-17 17:36:51 -0800

    Parse, display uptime

--- src/format.cpp
+++ src/format.cpp
@@ -10,4 +10,18 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds [[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  int hours, minutes;
+
+  minutes = seconds / 60;
+  seconds = seconds % 60;
+
+  hours = minutes / 60;
+  minutes = minutes % 60;
+
+  std::ostringstream oss;
+  oss << std::setfill('0') << hours << ":" << std::setw(2) << minutes << ":"
+      << std::setw(2) << seconds;
+
+  return oss.str();
+}
